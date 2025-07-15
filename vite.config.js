2c37f571d855989f07d514f05eb78fa36f27002c
Rodrigo <pancake911205@gmail.com>
2025-07-15 17:39:49 +0300

    change module

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import vite from "vitejs-plugin-refresh";
+import vite from "vitejs-plugin-react-refresh";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss(), vite()],
